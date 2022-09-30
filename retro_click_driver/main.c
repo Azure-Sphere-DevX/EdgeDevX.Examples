d08297434e07effb4c4ef61b6c6180964c243b49
Dave Glover <dglover@microsoft.com>
2022-09-30 19:04:23 +1000

    retro sample update

--- retro_click_driver/main.c
+++ retro_click_driver/main.c
@@ -28,7 +28,33 @@ int press_count = 0;
 as1115_t retro_click = {
     .interfaceId = I2C_MASTER, .handle = -1, .bitmap64 = 0, .keymap = 0, .debouncePeriodMilliseconds = 500};
 
-int fd_as1115 = -1;
+bool GetGpioState(GPIO_Value_Type *oldState)
+{
+    bool isGpioOn = false;
+    GPIO_Value_Type newState;
+
+    if (GPIO_GetValue(fd_button, &newState) == 0)
+    {
+        isGpioOn = (newState != *oldState) && (newState == GPIO_Value_Low);
+        *oldState = newState;
+    }
+    return isGpioOn;
+}
+
+void InitPeripheralsAndHandlers(void)
+{
+    fd_relay = GPIO_OpenAsOutput(GPIO_PIN, GPIO_OutputMode_PushPull, GPIO_Value_Low);
+    fd_button = GPIO_OpenAsInput(BUTTON_A);
+    retro_click.handle = I2CMaster_Open(retro_click.interfaceId);
+
+    as1115_init(retro_click.handle, &retro_click, 2);
+}
+
+void ClosePeripheralsAndHandlers(void)
+{
+    close(fd_relay);
+    close(fd_button);
+}
 
 int main(int argc, char *argv[])
 {
@@ -43,7 +69,7 @@ int main(int argc, char *argv[])
             GPIO_SetValue(fd_relay, (toggle_relay = !toggle_relay) ? GPIO_Value_High : GPIO_Value_Low);
             Log_Debug("Button pressed (%d)\n", ++press_count);
 
-            for (size_t i = 0; i < 1000; i++)
+            for (size_t i = 0; i < 1000000; i++)
             {
                 for (char i = ' '; i <= 'z'; i++)
                 {
@@ -69,37 +95,8 @@ int main(int argc, char *argv[])
 
             letter = ++letter > 'z' ? ' ' : letter;
         }
-
         nanosleep(&(struct timespec){0, 50 * ONE_MS}, NULL);
     }
 
     ClosePeripheralsAndHandlers();
-}
-
-void InitPeripheralsAndHandlers(void)
-{
-    fd_relay = GPIO_OpenAsOutput(GPIO_PIN, GPIO_OutputMode_PushPull, GPIO_Value_Low);
-    fd_button = GPIO_OpenAsInput(BUTTON_A);
-    retro_click.handle = I2CMaster_Open(retro_click.interfaceId);
-
-    as1115_init(retro_click.handle, &retro_click, 2);
-}
-
-void ClosePeripheralsAndHandlers(void)
-{
-    close(fd_relay);
-    close(fd_button);
-}
-
-bool GetGpioState(GPIO_Value_Type *oldState)
-{
-    bool isGpioOn = false;
-    GPIO_Value_Type newState;
-
-    if (GPIO_GetValue(fd_button, &newState) == 0)
-    {
-        isGpioOn = (newState != *oldState) && (newState == GPIO_Value_Low);
-        *oldState = newState;
-    }
-    return isGpioOn;
 }
